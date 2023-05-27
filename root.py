06d27dfde9a3d0526b1b6870fbd29d26bde283e4
RaduIord21 <nemus20@gmail.com>
2023-05-27 18:31:43 +0300

    added delete and create functions

--- root.py
+++
@@ -1,7 +0,0 @@
-class root:
-    def __init__(self, name, ext, first, size, flag):
-        self.name = name
-        self.ext = ext
-        self.first = first
-        self.size = size
-        self.flag = flag
